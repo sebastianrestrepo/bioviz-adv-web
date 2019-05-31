f6bbe1d9629409dfafb13a7946ddac83f669721a
Sebastián Restrepo <sebastianrestrepoq@gmail.com>
2019-05-31 16:28:06 -0500

    Updates

---
+++ precache-manifest.ff49cc1878131cc9f539e96656c8cce9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00234c01a2dc254f8592ac373079f63e",
+    "url": "/bioviz-adv-web/index.html"
+  },
+  {
+    "revision": "31dbaaee2f9daa3f9d3e",
+    "url": "/bioviz-adv-web/static/css/main.d721f947.chunk.css"
+  },
+  {
+    "revision": "1bda921af147b741cbea",
+    "url": "/bioviz-adv-web/static/js/2.aed840eb.chunk.js"
+  },
+  {
+    "revision": "31dbaaee2f9daa3f9d3e",
+    "url": "/bioviz-adv-web/static/js/main.80cb2ee2.chunk.js"
+  },
+  {
+    "revision": "4e07b0735666d335042b",
+    "url": "/bioviz-adv-web/static/js/runtime~main.e9418d32.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/bioviz-adv-web/precache-manifest.ff49cc1878131cc9f539e96656c8cce9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bioviz-adv-web/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
