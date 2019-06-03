5ec99cefdaa24eade446df3f12525d3c3fb0527f
Sebastián Restrepo <sebastianrestrepoq@gmail.com>
2019-06-02 23:59:10 -0500

    create project form drawed

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 import authStore from '../../stores/authStore';
+import projectsStore from '../../stores/projectsStore';
 import { observable } from 'mobx';
 import DashMenu from '../DashMenu/DashMenu';
 import Header from '../Header/Header';
@@ -13,6 +14,8 @@ interface ProjectsProps {
 @observer class Projects extends React.Component<ProjectsProps> {
 
     @observable signInForm: boolean = true;
+    @observable newProjectName: any = "";
+    @observable showNewProjectForm: boolean = false;
 
     constructor(props: any) {
         super(props);
@@ -27,16 +30,46 @@ interface ProjectsProps {
             <div className="projects-layout">
                 <Header />
                 <div className="projects-div">
+                    {(this.showNewProjectForm)
+                        ? <div className="create-project-div">
+                            <form className="create-project-form" onSubmit={
+                                (e: any) => {
+                                    e.preventDefault();
+                                    projectsStore.createProject(this.newProjectName, authStore.credentials.name, authStore.credentials.email);
+                                }}>
+                                <h1>NUEVO PROYECTO</h1>
+                                <input className="new-project-input" type="text" id="name" name="name" onChange={(e: any) => {
+                                    this.newProjectName = e.target.value;
+                                }} />
+                                <div className="new-project-btns">
+                                    <button className="cancel-btn" onClick={() => {
+                                        this.showNewProjectForm = false;
+                                    }}>
+                                        Cancelar
+                                    </button>
+                                    <input className="create-project-btn"
+                                        type="submit"
+                                        value="Crear"></input>
+                                </div>
+                            </form>
+                        </div>
+                        : ''}
+
                     <div className="no-projects-view">
                         <img src="./assets/birds-illustration.png" alt="" width="220" />
                         <h3>¡Bienvenido a BioViz, Sebastián!</h3>
                         <p>Aquí aparecerán tus proyectos para que puedas gestionarlos. <br></br> Crea carpertas y organiza.</p>
-                        <button>Crea un nuevo proyecto</button>
+                        <button onClick={() => {
+                            this.showNewProjectForm = true;
+                        }}>Crea un nuevo proyecto</button>
                     </div>
                 </div>
-                <button className="add-project-float-btn">
-                <img src="./assets/add-icon.png" alt="" width="40" />
-            </button>
+                <button className="add-project-float-btn"
+                    onClick={() => {
+                        this.showNewProjectForm = true;
+                    }}>
+                    <img src="./assets/add-icon.png" alt="" width="40" />
+                </button>
             </div>
 
         </div>);
---
+++ src/stores/projectsStore.ts
@@ -0,0 +1,39 @@
+import { observable, autorun, toJS, configure, action, computed, extendObservable } from 'mobx';
+import firebase from 'firebase';
+import { History } from 'history';
+import authStore from '../stores/authStore';
+import { db, storage, auth } from '../firebaseConfig';
+import { observer } from 'mobx-react';
+
+class ProjectsStore {
+
+    @observable username: any = "";
+    @observable messages: any[] = [];
+    @observable text: any = "";
+    @observable profile_photo: any = "";
+
+    constructor() {
+        autorun(() => {
+            var userId: any = authStore.user.uid;
+            var database = db.ref('users/' + userId + '/username');
+            database.once('value').then((snapshot: any) => {
+                console.log('snapshote value', snapshot.val());
+                this.username = snapshot.val();
+            });
+        });
+    }
+
+    @action createProject(projectName, username, email){
+        db.ref('projects/' + authStore.user.uid).set({
+            projectName: projectName,
+            username: username,
+            email: email,
+          });
+    }
+
+}
+
+
+const projectsStore = new ProjectsStore();
+
+export default projectsStore;
