1f79d3f590927e92b11e50292f10b03b29f31179
Paula <paulaagarcia1@gmail.com>
2019-08-25 13:17:03 -0500

    Home redirect changed by Tagging Section component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Route, Link, BrowserRouter } from "react-route
 import { HashRouter } from "react-router-dom";
 import Projects from '../Projects/Projects';
 import Chat from '../Chat/Chat';
+import Tagging from '../Tagging/Tagging';
 
 class App extends Component {
   constructor(props: {}) {
@@ -17,7 +18,7 @@ class App extends Component {
     return (<div className="App">
       <HashRouter basename="/bioviz-adv-web">
         <Route exact path="/" component={Login} />
-        <Route exact path="/home" component={Home} />
+        <Route exact path="/home" component={Tagging} />
         <Route exact path="/projects" component={Projects} />
         <Route exact path="/chat" component={Chat} />
       </HashRouter>
---
+++ src/components/Tagging/Tagging.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './_Tagging.scss'
+import { observer } from 'mobx-react';
+import authStore from '../../stores/authStore';
+import { observable, autorun } from 'mobx';
+import DashMenu from '../DashMenu/DashMenu';
+
+interface TaggingProps {
+    history: any
+}
+
+@observer class Tagging extends React.Component<TaggingProps> {
+
+    @observable signInForm: boolean = true;
+
+    constructor(props: any) {
+        super(props);
+        autorun(() => {
+            if (!authStore.isLogged) {
+                props.history.push("/");
+            }
+        });
+    }
+
+    render() {
+        return (<div className="tagging">
+            <DashMenu />
+        </div>);
+    }
+}
+
+export default observer(Tagging);
