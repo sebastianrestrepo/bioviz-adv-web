5aaf1fb7ea1c3bdae8071eb0791211963380b27b
Paula <paula.garcia1@correo.icesi.edu.co>
2019-11-19 10:20:38 -0500

    Auto stash before revert of "Merge branch 'feature/project-creation-fb' into develop"

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,7 @@ import './App.scss';
 import Login from '../Login/Login';
 import Home from '../Home/Home';
 import { observer } from 'mobx-react';
-import { BrowserRouter as Router, Route, Link, BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { HashRouter } from "react-router-dom";
 import Projects from '../Projects/Projects';
 import Tagging from '../Tagging/Tagging';
@@ -21,8 +21,8 @@ class App extends Component {
     return (<div className="App">
       <HashRouter>
         <Route exact path="/" component={Login} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/projects" component={Projects} />
+        <Route  path="/home" component={Home} />
+        <Route  path="/projects" component={Projects} />
       </HashRouter>
     </div>
     );
