abf5b0480918281c0eb6e60009ec744a3c8ffb9f
Sebastián Restrepo <sebastianrestrepoq@gmail.com>
2019-11-19 23:57:03 -0500

    spectro selection tsx created

---
+++ src/components/Tagging/SpectroSel.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './_SpectroComp.scss'
+import WaveSurfer from 'wavesurfer.js';
+import Spectrogram from 'wavesurfer.js/dist/plugin/wavesurfer.spectrogram.js';
+import Timeline from 'wavesurfer.js/dist/plugin/wavesurfer.timeline.js';
+import Minimap from 'wavesurfer.js/dist/plugin/wavesurfer.minimap';
+import Regions from 'wavesurfer.js/dist/plugin/wavesurfer.regions';
+import Cursor from 'wavesurfer.js/dist/plugin/wavesurfer.cursor';
+import toolsStore from '../../stores/toolsStore';
+
+const SpectroSel = () => {
+
+    // Selección Spectro
+
+    const containerSpecSel: any = React.useRef();
+
+}
+
+export default SpectroSel;
