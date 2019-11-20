1264a6fcf8afa4b6d6310ca032df1b1b7a0e6995
Sebastián Restrepo <sebastianrestrepoq@gmail.com>
2019-11-19 20:44:44 -0500

    gen spec functional, region inciating

--- src/components/Tagging/SpectroComp.tsx
+++ src/components/Tagging/SpectroComp.tsx
@@ -3,20 +3,30 @@ import './_SpectroComp.scss'
 import WaveSurfer from 'wavesurfer.js';
 import Spectrogram from 'wavesurfer.js/dist/plugin/wavesurfer.spectrogram.js';
 import Timeline from 'wavesurfer.js/dist/plugin/wavesurfer.timeline.js';
+import Minimap from 'wavesurfer.js/dist/plugin/wavesurfer.minimap';
+import Regions from 'wavesurfer.js/dist/plugin/wavesurfer.regions';
+import Cursor from 'wavesurfer.js/dist/plugin/wavesurfer.cursor';
 
 const SpectroComp = () => {
 
+    // General Spectrogram
     const containerRef: any = React.useRef();
     const containerSpecRef: any = React.useRef();
     const containerTimelineRef: any = React.useRef();
     const wsRef: any = React.useRef();
     const sliderRef: any = React.useRef();
+    const containerRegion: any = React.useRef();
+
+    // Selección Spectro
+    const containerSpecSel: any = React.useRef();
 
     React.useEffect(() => {
         wsRef.current = WaveSurfer.create({
             container: containerRef.current,
-            waveColor: 'rgba(175, 234, 125, 0.4)',
-            progressColor: '#89D46E',
+            waveColor: 'rgba(175, 234, 125, 0.1)',
+            progressColor: 'rgba(137, 212, 110, 0.1)',
+            cursorWidth: 4,
+            cursorColor: '#AFEA7D',
             height: 120,
             plugins: [
                 Spectrogram.create({
@@ -27,7 +37,35 @@ const SpectroComp = () => {
                 }),
                 Timeline.create({
                     container: containerTimelineRef.current
-                })
+                }),
+                Minimap.create({
+                    container: containerSpecSel.current,
+                    waveColor: '#777',
+                    progressColor: '#222',
+                    height: 50
+                }),
+                Cursor.create({
+                    showTime: true,
+                    opacity: 0.8,
+                    customShowTimeStyle: {
+                        'background-color': '#000',
+                        color: '#fff',
+                        padding: '2px',
+                        'font-size': '10px'
+                    }
+                }),
+                Regions.create({
+                    regions: [
+                        {
+                            container: containerRegion.current,
+                            id: 'rg',
+                            start: 1,
+                            end: 3,
+                            loop: false,
+                            color: 'rgba(60, 238, 89, 0.2)',
+                        }
+                    ],
+                }),
             ]
         });
 
@@ -45,6 +83,23 @@ const SpectroComp = () => {
 
     const handlePlayPause = () => {
         wsRef.current.playPause();
+        saveRegions();
+    }
+
+    function saveRegions() {
+        localStorage.regions = JSON.stringify(
+            Object.keys(wsRef.current.regions.list).map(function(id) {
+                var region = wsRef.current.regions.list[id];
+                console.log(region);
+                console.log(region.start);
+                return {
+                    start: region.start,
+                    end: region.end,
+                    attributes: region.attributes,
+                    data: region.data
+                };
+            })
+        );
     }
 
     const handleSliderChange = () => {
@@ -55,6 +110,13 @@ const SpectroComp = () => {
     }
 
     return (<div className="container">
+
+        <div id="timeline" ref={containerTimelineRef} />
+        <div id="waveform" ref={containerRef}>
+            <div id="wave-spectrogram" ref={containerSpecRef} />
+        </div>
+        <div id="minimap" ref={containerSpecSel} />
+
         <div className="div-btns">
             <button id="color-change" onClick={handleColorChange}>Cambiar color de onda</button>
             <button id="play-pause" onClick={handlePlayPause}>Reproducir/Pausar</button>
@@ -64,11 +126,7 @@ const SpectroComp = () => {
                 <img src="./assets/temp-icons/zoom-in-icon.png" width="30" height="30" />
             </div>
         </div>
-
-        <div id="waveform" ref={containerRef}>
-            <div id="wave-spectrogram" ref={containerSpecRef} />
-        </div>
-        <div id="timeline" ref={containerTimelineRef} />
+        
     </div>);
 }
 
